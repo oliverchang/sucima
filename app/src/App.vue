6fc53b2dd57bafd00dae720ecc1a72d31f47a906
Oliver Chang <oliver.changs@gmail.com>
2019-08-10 14:49:47 +1000

    Change theme

--- app/src/App.vue
+++ app/src/App.vue
@@ -11,7 +11,7 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 Vue.use(BootstrapVue);
 
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootswatch/dist/darkly/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
